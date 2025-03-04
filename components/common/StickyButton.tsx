3b9d53daa43ebecad623e3d2a1f1251443122169
bazabizi <bazabizi3@gmail.com>
2025-03-05 00:29:28 +0300

    change the size of the stickybutton for mobile view

--- components/common/StickyButton.tsx
+++ components/common/StickyButton.tsx
@@ -8,8 +8,8 @@ const StickyButton = () => {
       <button
       onClick={() => router.push("/contact")}
        className="fixed md:bottom-32 bottom-20 right-5 text-white font-semibold px-6 py-3 z-50 ">
-        <div className="md:w-32 w-24 h-24 md:h-32 bg-primary bg-gradient-to-t from-primary via-primary to-secondary rounded-tl-[80px] rounded-tr-[80px] rounded-bl-[80px] rounded-br-[2px] flex justify-center items-center relative">
-          <span className="absolute text-white md:text-xs text-[10px]">JETZT KONTAKTIEREN</span>
+        <div className="md:w-24 w-16 h-16 md:h-24 bg-primary bg-gradient-to-t from-primary via-primary to-secondary rounded-tl-[80px] rounded-tr-[80px] rounded-bl-[80px] rounded-br-[2px] flex justify-center items-center relative">
+          <span className="absolute text-white md:text-[10px] text-[8px]">JETZT KONTAKTIEREN</span>
         </div>
       </button>
   );
